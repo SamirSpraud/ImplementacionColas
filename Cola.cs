da5a03c033bdaeb2e507e48ea0df246db1835f59
Samir Spraud <samirspraud0209@gmail.com>
2024-05-28 22:08:46 -0400

    First commit

---
+++ Cola.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colas
+{
+    internal class Cola
+    {
+        public Nodo primero;
+        public Nodo ultimo;
+
+        public Cola() {
+            primero = ultimo = null;
+        }
+
+        public void Encolar(int valor)
+        {
+            Nodo aux = new Nodo();
+            aux.info = valor;
+            if (primero == null) { 
+                primero = ultimo=aux;
+                aux.sgte = null;
+            }
+            else
+            {
+                ultimo.sgte = aux;
+                aux.sgte = null;
+                ultimo = aux;
+            }
+
+        }
+
+        public void Desencolar()
+        {
+            if(primero == null)
+            {
+                Console.WriteLine("La cola esta vacia");
+            }
+            else
+            {
+                primero = primero.sgte;
+            }
+        }
+
+        public int DesencolarValor()
+        {
+            int valor = 0;
+            if (primero == null)
+            {
+                Console.WriteLine("La cola esta vacia");
+            }
+            else
+            {
+                valor = primero.info;
+                primero = primero.sgte;
+            }
+            return valor;
+        }
+
+        public void Show()
+        {
+            if (primero == null)
+            {
+                Console.WriteLine("La cola esta vacia");
+            }
+            else
+            {
+                Nodo puntero;
+                puntero = primero;
+                do {
+                    Console.WriteLine("{0}\t", puntero.info);
+                    puntero = puntero.sgte;
+                }while(puntero != null);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Colas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Cola cola = new Cola();
+            Console.WriteLine("Colocando 5 elementos en la cola");
+            cola.Encolar(3);
+            cola.Encolar(34);
+            cola.Encolar(56);
+            cola.Encolar(10);
+            cola.Encolar(7);
+            cola.Show();
+
+            Console.WriteLine("Retirando elementos de la cola");
+            cola.Desencolar();
+            cola.Show();
+            cola.Desencolar();
+            cola.Show();
+
+            Console.WriteLine("Se va a retirar un nodo mas, con el valor de {0}", cola.DesencolarValor());
+            cola.Show();
+            Console.ReadLine();
+        }
+    }
+}
